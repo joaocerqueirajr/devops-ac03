3861f296cdd02dbdf52c3452f9dada45e2ec01a3
João Carlos <42190825+joaocerqueirajr@users.noreply.github.com>
2018-09-06 22:19:04 -0300

    Update testes.py

--- src/testes.py
+++ src/testes.py
@@ -7,7 +7,7 @@ jogo = jogovelha.inicializar()
 if len(jogo) != 3:
    erroInicializar = True
 else:
-   for linha in jogo:
+   for linha in jogo
        if len(linha) != 3:
             erroInicializar = True
        else:
